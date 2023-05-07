f87847e410d4f2fdff768371419455cdad5132b8
Wagner de Lima <wagner_de_lima@epam.com>
2023-05-08 01:03:22 +0200

    feat: add admin.py and other project files.

---
+++ tank/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from tank.models import Tank, TankVolume, AverageSales
+
+# Register your models here.
+admin.site.register(Tank)
+admin.site.register(TankVolume)
