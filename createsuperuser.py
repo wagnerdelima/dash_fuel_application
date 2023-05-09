2c672536101ccfb61401a29a5a7ac11482c6357d
Wagner de Lima <wagner_de_lima@epam.com>
2023-05-09 22:51:32 +0200

    feat: create super user as default.

---
+++ createsuperuser.py
@@ -0,0 +1,18 @@
+from django.db import IntegrityError
+from django.contrib.auth.models import User
+
+import environ # This package import from 'django-environ' https://pypi.org/project/django-environ/
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+try:
+    superuser = User.objects.create_superuser(
+        username=env('SUPER_USER_NAME', default="admin"),
+        email=env('SUPER_USER_EMAIL', default="admin@getdashfuel.com"),
+        password=env('SUPER_USER_PASSWORD', default="mypass"))
+    superuser.save()
+except IntegrityError:
+    print(f"Super User with username {env('SUPER_USER_NAME')} is already exit!")
+except Exception as e:
+    print(e)
