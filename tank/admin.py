650b5bd405a95b93d0e2b04fd1488ec106e5d36f
Wagner de Lima <wagner_de_lima@epam.com>
2023-05-09 21:19:10 +0200

    feat: add custom admin models for tabled views.

--- tank/admin.py
+++ tank/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 
-from tank.models import Tank, TankVolume
+from tank.models import Tank, TankVolume, AverageSales
+
+
+@admin.register(AverageSales)
+class AverageAdmin(admin.ModelAdmin):
+    list_display = ('tank', 'avg_sales', 'calculated_at',)
+
+
+@admin.register(TankVolume)
+class AverageAdmin(admin.ModelAdmin):
+    list_display = ('tank', 'volume', 'created_at', 'updated_at')
 
 # Register your models here.
 admin.site.register(Tank)
-admin.site.register(TankVolume)
