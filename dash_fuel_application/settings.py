6deb2454aba3aa366f563d7e80498bd3401d26b1
Wagner de Lima <wagner_de_lima@epam.com>
2023-05-26 20:27:03 +0200

    refac: add allowed hosts.

--- dash_fuel_application/settings.py
+++ dash_fuel_application/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = getenv('SECRET_KEY', 'test')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
